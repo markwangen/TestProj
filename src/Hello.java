10288c4b254f5e2516237b16f463a7319d80321d
mark.wang <mark.wang.en@gmail.com>
2013-05-18 00:31:09 +0800

    Just the test case.

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world, check now");
+		System.out.println(calculater(6.5, 8.8));
+	}
+	
+	public static double calculater(double nums, double numd) {
+		return nums * numd;
+	}
+
+}
