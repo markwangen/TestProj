9e7ff060ec857bb98f6f1392a686987887c868af
mark.wang <mark.wang.en@gmail.com>
2013-11-30 02:11:03 +0800

    Just add a blank space

--- src/Hello.java
+++ src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world, check now");
+		System.out.println("Hello world, check now ");
 		System.out.println(calculater(6.5, 8.8));
 	}
 	
